ba5ddb652e150db9a39ed4ed9db842a19a3098c7
Vicky01396 <6345@holbertonstudents.com>
2023-02-17 23:28:34 -0800

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry pint
+ *
+ * Return: Always 0 (Seccess)
+ */
+int main(void)
+{
+	int ls;
+
+	for (ls = 10 ; ls < 20 ; ls++)
+	{
+		putchar((ls % 10) + '0');
+
+		if (ls != 19)
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
