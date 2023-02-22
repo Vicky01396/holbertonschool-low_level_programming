83ed4afa4f50274a64b57ceaa10aff128bb63abf
Vicky01396 <6345@holbertonstudents.com>
2023-02-21 21:20:10 -0800

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - calculates the integer value of a number
+ * @n: variable
+ *
+ * Return: -n or n
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
