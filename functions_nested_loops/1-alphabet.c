c0e382d0e865d0221826a57ce0680c05e92971c8
Vicky01396 <6345@holbertonstudents.com>
2023-02-21 17:46:42 -0800

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
-/*
- * print_alphabet - print alphabet
+/**
+ * print_alphabet - prints all the alphabet
  *
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
 	char letter = 'a';
 
-	while (letter <= 'z');
+	while (letter <= 'z')
 	{
 		_putchar(letter);
 			letter++;
