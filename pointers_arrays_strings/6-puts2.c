2f2a6742116cc48f84a2187e27cb54d10c44f621
Vicky01396 <6345@holbertonstudents.com>
2023-02-28 10:33:44 -0800

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts2 - function that prints every second character of a string
+ * @str: variable
+ */
+void puts2(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str + = 2;
+	}
+	_putchar('\n');
+}
