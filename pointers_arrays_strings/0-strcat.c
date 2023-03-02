9bb08584c7098e4f4681b883197a5cb143f457f3
Vicky01396 <6345@holbertonstudents.com>
2023-03-02 07:04:08 -0800

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcat - function that concatenates two strings.
+ * @dest: of the overwritten string
+ * @src: main string
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int h;
+	int w;
+
+	for (h = 0; dest[h] != '\0'; h++)
+	{
+	}
+	for (w = 0; (dest[h + w] = *src++) != '\0'; w++)
+	{
+	}
+	return (0);
+}
