13296b91de439961328b6ea9b58762907642c062
Vicky01396 <6345@holbertonstudents.com>
2023-03-07 12:37:30 -0800

    6-is_prime_number.c

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * aux - auxiliar function
+ * @x: an int
+ * @y: another int
+ * Return: function
+ */
+int aux(int x, int y)
+{
+	if (x < 2)
+	{
+		return (0);
+	}
+	if (y == 1)
+	{
+		return (1);
+	}
+	if (x % y == 0)
+	{
+		return (0);
+	}
+	return (aux(x, y - 1));
+}
+
+/**
+ * is_prime_number - finds prime numbers
+ * @n: the  number to check
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	return (aux(n, n - 1));
+}
