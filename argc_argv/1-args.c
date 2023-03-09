9d2c07c0ff68b3d9ca259ce025abfe4ec7228994
Vicky01396 <6345@holbertonstudents.com>
2023-03-09 15:07:12 -0800

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: void
+ * @argv: string
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
