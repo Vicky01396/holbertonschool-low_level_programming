925c4f9f8280d973ae328c679e5c3f30334d9361
Vicky01396 <6345@holbertonstudents.com>
2023-03-09 18:06:09 -0800

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - entry point
+ * @argc: void
+ * @argv: string
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int j, v, add = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (j = 1; j < argc; j++)
+	{
+		for (v = 0; argv[j][v]; v++)
+		{
+			if (isdigit(argv[j][v]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[j]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
