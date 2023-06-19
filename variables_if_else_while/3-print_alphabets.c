c652611aedc69ea2bc4a961c9d947c2631a093aa
Vicky01396 <6345@holbertonschool.com>
2023-06-19 01:27:07 -0700

    task_3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp = 'a';
+	char ALP = 'A';
+
+	while (alp <= 'z')
+	{
+		putchar(alp);
+		alp++;
+	}
+	while (ALP <= 'Z')
+	{
+		putchar(ALP);
+		ALP++;
+	}
+	return (0);
+}
