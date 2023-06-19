1aa396aef47d9ce94394fde75d7f75edb2274e63
Vicky01396 <6345@holbertonschool.com>
2023-06-19 14:54:59 -0700

    task_0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  main - start
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
