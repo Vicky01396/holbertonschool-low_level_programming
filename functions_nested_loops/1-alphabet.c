8c270837a1f4ac1d4cc7a5c6f994075af718136e
Vicky01396 <6345@holbertonschool.com>
2023-06-19 19:41:17 -0700

    task_1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all the alphabet
+ */
+void print_alphabet(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		_putchar(alp);
+		alp++;
+	}
+	_putchar('\n');
+}
