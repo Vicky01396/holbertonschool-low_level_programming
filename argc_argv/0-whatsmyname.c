cb45f75f99c1ac472c1b65fbe53935e8acb57602
Vicky01396 <6345@holbertonschool.com>
2023-07-02 23:12:15 -0700

    task_0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints tha name of the funtion
+ * @argc: how many arguments are
+ * @argv: the string
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
