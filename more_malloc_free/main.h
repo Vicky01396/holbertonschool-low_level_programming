3c8ec54beecadd128b51b1c43343150b399b3931
Vicky01396 <6345@holbertonschool.com>
2023-07-07 12:42:17 -0700

    main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -9,9 +9,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _putchar(char c);
 
-char *_memset(char *s, char b, unsigned int n);
-char *_strcat(char *dest, char *src);
-int _strlen(char *s);
-char *_strncat(char *dest, char *src, int n);
-
 #endif
