d4f44f94b03e0a379f00c2c44396fa8b5d17d755
Vicky01396 <6345@holbertonschool.com>
2023-07-07 13:16:44 -0700

    task_2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - print file of original file
+ * Return: 0 always
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
