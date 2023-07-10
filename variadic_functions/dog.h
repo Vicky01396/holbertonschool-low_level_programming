7ab3e05f3821ffb538333542b09642d470096229
Vicky01396 <6345@holbertonschool.com>
2023-07-10 12:33:09 -0700

    dog.h

---
+++ variadic_functions/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+/**
+ * struct dog - the struct of a dog
+ * @name: the name
+ * @age: the age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
