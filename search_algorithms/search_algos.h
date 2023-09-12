f7c5d8d4ff7b668d11175d081a43c4a74fd3dc59
Vicky <63452@holbertonstudents.com>
2023-09-12 11:16:04 -0700

    fix betty

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
